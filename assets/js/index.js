11cfe90db77a70c8bd4d5b1a359bced464834834
Lucasf5 <lucasfpnt@gmail.com>
2022-02-09 12:30:27 -0300

    atualizacao 09/02

--- assets/js/index.js
+++ assets/js/index.js
@@ -137,3 +137,18 @@ function initAnimacao() {
 }
 
 initAnimacao()
+
+
+const botaoFechar = document.querySelector('.fechar')
+const modal = document.querySelector('.modal')
+window.onload = () => {
+
+    botaoFechar.addEventListener('click', () => {
+        modal.style.display = 'none'
+    })
+    modal.addEventListener('click', (evento) => {
+        if (evento.target == modal) {
+            modal.style.display = 'none'
+        }
+    })
+}
