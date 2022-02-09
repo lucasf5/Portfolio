8727cfd3cdc8d730d46c9a4b0784b9d85874ee29
Lucasf5 <lucasfpnt@gmail.com>
2022-02-09 16:15:59 -0300

    implementacao da animacao ao scroll na parte das skils e mudanca do valor da altura do windows para telas maiores que 1080px

--- assets/js/index.js
+++ assets/js/index.js
@@ -112,32 +112,44 @@ fonteDiminuir.addEventListener('click', () => {
     })
 })
 
-// animacao
+// ANIMACAO MOVIMENTACAO
 
 const sections = document.querySelectorAll(".js-scroll")
-const windowMetade = window.innerHeight * 0.7
-function initAnimacao() {
-    if (sections.length) {
+const imagens = document.querySelectorAll('.skils__imagem')
 
-        function animaScroll() {
-            sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top - windowMetade
-                if (sectionTop < 0) {
-                    section.classList.add('ativo')
-                } else {
-                    section.classList.remove('ativo')
-                }
-            })
+const windowmetadeAlto = window.innerWidth
+let windowMetade = 0
 
-        }
-        animaScroll()
-
-        window.addEventListener('scroll', animaScroll)
-    }
+if (windowmetadeAlto <= 1080) {
+    windowMetade = window.innerHeight * 0.875
+} else if (windowmetadeAlto > 1080){
+    windowMetade = window.innerHeight * 0.8
 }
 
-initAnimacao()
 
+    function animaScroll() {
+        sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top - windowMetade
+            if (sectionTop < 0) {
+                section.classList.add('ativo')
+            } else {
+                section.classList.remove('ativo')
+            }
+        })
+        imagens.forEach((imagem) => {
+            const sectionTop = imagem.getBoundingClientRect().top - windowMetade
+            if (sectionTop < 0) {
+                imagem.classList.add('ativo')
+            } else {
+                imagem.classList.remove('ativo')
+            }
+        })
+    }
+
+    window.addEventListener('scroll', animaScroll)
+
+
+// MODAL
 
 const botaoFechar = document.querySelector('.fechar')
 const modal = document.querySelector('.modal')
